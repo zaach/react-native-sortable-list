91e1f685ff172a0493a035fd7cb06071bd90265c
Timur Gibadullin <tim.gibadullin@gmail.com>
2016-11-16 16:21:56 +0300

    Implement ability to update data

--- examples/Basic/index.ios.js
+++ examples/Basic/index.ios.js
@@ -29,48 +29,38 @@ const data = {
     image: 'https://placekitten.com/200/201',
     text: 'Jasper',
   },
-  2: {
-    image: 'https://placekitten.com/200/202',
-    text: 'Pepper',
-  },
-  3: {
-    image: 'https://placekitten.com/200/203',
-    text: 'Oscar',
-  },
-  4: {
-    image: 'https://placekitten.com/200/204',
-    text: 'Dusty',
-  },
-  5: {
-    image: 'https://placekitten.com/200/205',
-    text: 'Spooky',
-  },
-  6: {
-    image: 'https://placekitten.com/200/210',
-    text: 'Kiki',
-  },
-  7: {
-    image: 'https://placekitten.com/200/215',
-    text: 'Smokey',
-  },
-  8: {
-    image: 'https://placekitten.com/200/220',
-    text: 'Gizmo',
-  },
-  9: {
-    image: 'https://placekitten.com/220/239',
-    text: 'Kitty',
-  },
+
 };
 
 class Basic extends Component {
+  state = {
+    data,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        data: {
+          ...this.state.data,
+          2: {
+            image: 'https://placekitten.com/200/202',
+            text: 'Pepper',
+          },
+          3: {
+            image: 'https://placekitten.com/200/203',
+            text: 'Oscar',
+          },
+        }
+      })
+    }, 2000)
+  }
   render() {
     return (
       <View style={styles.container}>
         <SortableList
           style={styles.list}
           contentContainerStyle={styles.contentContainer}
-          data={data}
+          data={this.state.data}
           renderRow={this._renderRow} />
       </View>
     );
